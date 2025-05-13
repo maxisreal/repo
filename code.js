0300fe50820b19a0c6b111f0f83baad9fb9ebda7
maxisreal <145690663+maxisreal@users.noreply.github.com>
2025-05-13 14:28:13 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,34 +1,17 @@
 import mysql.connector
 
-
 connection = mysql.connector.connect(user='maximillianw6',
                                     password='203892179',
                                     host='10.8.37.226',
                                     database='maximillianw6_db')
 
-
 cursor = connection.cursor()
-
-
-
-
 val = int(input("Enter student ID: "))
-
-
-
-
 query = ""
-
-
-
-
 cursor.execute(query)
 for row in cursor:
    print(row[0], row[1])
    a.append(row)
-
-
-
 
 cursor.close()
 connection.close()
